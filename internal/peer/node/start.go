555ce93e957cf53b93e105687982fe324a1261b4
Alvin(Xinyao) Sun <lucklyric@gmail.com>
2020-12-28 13:12:59 -0800

    [Feature] Add evidence custom system chaincode

--- internal/peer/node/start.go
+++ internal/peer/node/start.go
@@ -74,6 +74,7 @@ import (
 	"github.com/hyperledger/fabric/core/scc/cscc"
 	"github.com/hyperledger/fabric/core/scc/lscc"
 	"github.com/hyperledger/fabric/core/scc/qscc"
+	"github.com/hyperledger/fabric/core/scc/evidence"
 	"github.com/hyperledger/fabric/core/transientstore"
 	"github.com/hyperledger/fabric/discovery"
 	"github.com/hyperledger/fabric/discovery/endorsement"
@@ -592,6 +593,7 @@ func serve(args []string) error {
 		"lscc":       {},
 		"qscc":       {},
 		"cscc":       {},
+		"evidence":   {},
 		"_lifecycle": {},
 	}
 
@@ -697,6 +699,7 @@ func serve(args []string) error {
 		factory.GetDefault(),
 	)
 	qsccInst := scc.SelfDescribingSysCC(qscc.New(aclProvider, peerInstance))
+	evidenceInst := scc.SelfDescribingSysCC(evidence.New())
 
 	pb.RegisterChaincodeSupportServer(ccSrv.Server(), ccSupSrv)
 
@@ -744,7 +747,7 @@ func serve(args []string) error {
 	}
 
 	// deploy system chaincodes
-	for _, cc := range []scc.SelfDescribingSysCC{lsccInst, csccInst, qsccInst, lifecycleSCC} {
+	for _, cc := range []scc.SelfDescribingSysCC{lsccInst, csccInst, qsccInst, lifecycleSCC, evidenceInst} {
 		if enabled, ok := chaincodeConfig.SCCAllowlist[cc.Name()]; !ok || !enabled {
 			logger.Infof("not deploying chaincode %s as it is not enabled", cc.Name())
 			continue
